0a5132f7d72e9f25bfaa1ce915fb38480b5466e2
Abdoulaye Aliou Sall <gallebaba71@gmail.com>
2025-02-23 21:10:10 +0000

    kafka config sending notification implemented

--- services/order/src/main/java/com/babagl/ecommerce/order/OrderService.java
+++ services/order/src/main/java/com/babagl/ecommerce/order/OrderService.java
@@ -3,6 +3,8 @@ package com.babagl.ecommerce.order;
 import com.babagl.ecommerce.customer.CustomerClient;
 import com.babagl.ecommerce.customer.CustomerResponse;
 import com.babagl.ecommerce.exception.BusinessException;
+import com.babagl.ecommerce.kafka.OrderConfirmation;
+import com.babagl.ecommerce.kafka.OrderProducer;
 import com.babagl.ecommerce.orderline.OrderLineRequest;
 import com.babagl.ecommerce.orderline.OrderLineService;
 import com.babagl.ecommerce.product.ProductClient;
@@ -19,13 +21,14 @@ public class OrderService {
     private final ProductClient productClient;
     private final OrderMapper mapper;
     private final OrderLineService orderLineService;
+    private final OrderProducer orderProducer;
     public Integer createOrder(OrderRequest request) {
         //check the costumer --> OpenFeign;
         CustomerResponse customer = this.customerClient.findCustomerById(request.customerId())
                 .orElseThrow(()-> new BusinessException("On ne peut pas creer une commande:: Le client dont l'ID est egale a "+request.customerId()+ "n'existe pas"));
 
         //purchase the products --> product-ms(RestTemplate)
-        this.productClient.purchaseProducts(request.products());
+        var purchasedProduct = this.productClient.purchaseProducts(request.products());
 
 
         //persist order
@@ -44,11 +47,21 @@ public class OrderService {
             );
         }
 
-        //start payment process
+        //todo start payment process
 
         //send the order confirmation
 
         //send the order confirmation  --> notification-ms (kafka)
-        return null;
+        orderProducer.sendOrderConfirmation(
+                new OrderConfirmation(
+                        request.reference(),
+                        request.amount(),
+                        request.paymentMethod(),
+                        customer,
+                        purchasedProduct
+
+                )
+        );
+        return order.getId();
     }
 }
